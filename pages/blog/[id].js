c6f2344ce47feab9760f9205c2c71e2fa7a02861
unknown <robin.luongo@gmail.com>
2020-02-05 00:23:37 -0500

    fixing prerender issue

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -3,21 +3,27 @@ import Head from 'next/head';
 import fetch from 'isomorphic-unfetch';
 
 function Post(props) {
-    return (
-        <>
-        <Head>
-            <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        </Head>
-        <article>
-            <h1>{props.data.title}</h1>
-        </article>
-        </>
-    )
+    if (props.data != '') {
+        return (
+            <>
+            <Head>
+                <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+            </Head>
+            <article>
+                <h1>{props.data.title}</h1>
+            </article>
+            </>
+        )
+    } else {
+        return (
+            <></>
+        )
+    }
 }
 
 Post.getInitialProps = async function({query}) {
     const res = await fetch(`http://localhost:3000/content/${query.id}.json`);
-    const data = await res.json();
+    const data = query.id ? await res.json() : ''
   
     console.log(`Data fetched. Count: ${data}`);
   
