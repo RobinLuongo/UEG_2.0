227ec08fb49171b812af03ac25d642b9134093bd
unknown <robin.luongo@gmail.com>
2020-02-05 22:16:55 -0500

    gonna test now

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -15,23 +15,41 @@ function Post(props) {
                 <article>
                     <h1 className="blog-title">{props.data.title}</h1>
                     <div className="dateline">by {props.data.author} on {props.data.date}</div>
+                    <div className="br"></div>
                     <ReactMarkdown source={props.data.body} className="blog-content"/>
                 </article>
-                <style jsx>{`
+                <style>{`
                     article {
                         margin: auto;
-                        width: 900px;
+                        max-width: 800px;
                     }
                     .blog-title {
-                        font-size: 40px;
+                        font-size: 46px;
                         color: ${theme.colors["dark-blue"]};
                         margin-bottom: 5px;
                     }
                     .dateline {
                         
                     }
+                    .br {
+                        border-bottom: 2px solid #80808057;
+                        margin: 10px 0px;
+                    }
                     .blog-content {
                         margin-top: 20px;
+                    }
+                    .blog-content > h1 {
+                        font-size: 34px;
+                        color: ${theme.colors["reg-blue"]};
+                    }
+                    .blog-content > h2 {
+                        font-size: 26px;
+                        color: ${theme.colors["reg-blue"]};
+                    }
+                    .blog-content > p {
+                        font-size: 22px;
+                        line-height: 1.5;
+                        color: #828282;
                     }
                 `}</style>
             </>
