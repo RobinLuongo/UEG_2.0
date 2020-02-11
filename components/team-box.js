0ee4bc46377fe5fb388c58e82e91012e42ade2ec
unknown <robin.luongo@gmail.com>
2020-02-11 11:25:52 -0500

    our team with circles

--- components/team-box.js
+++ components/team-box.js
@@ -3,8 +3,10 @@ export default function TeamBox(props) {
         <div className={`${props.className} box-container`}>
             {
                 props.imgRef ? 
-                <div className='headshot'>
-                    {/* <img src={props.imgRef} alt={props.name + ' headshot'} className='headshot'></img> */}
+                <div className='circle'>
+                    <div className='headshot'>
+                        {/* <img src={props.imgRef} alt={props.name + ' headshot'} className='headshot'></img> */}
+                    </div>
                 </div>
                 : ''
             }
@@ -23,22 +25,32 @@ export default function TeamBox(props) {
                     width: 250px;
                     height: 250px;
                     border-radius: 50%;
-                    margin: 0px auto 15px auto;
+                    margin: auto;
                     background-image: url(${props.imgRef});
+                    box-shadow: 0px 0px 20px 0px #4c4c4c78;
                 }
                 .alex .headshot {
                     background-size: 110%;
                     background-position: top -46px right 5px;
                 }
                 .jesse .headshot {
-                    background-size: 100%;
-                    transform: rotate(90deg);
-                    background-position: top 37px left 0px;
+                    background-size: 120%;
+                    background-position: top -2px left -19px;
                 }
                 .robin .headshot {
                     background-size: 160%;
                     background-position: top 0px right -68px;
                 }
+                .circle {
+                    background-image: url(/images/circle.png);
+                    background-size: 100%;
+                    background-position: top 1px left -1px;
+                    background-repeat: no-repeat;
+                    height: 300px;
+                    width: 300px;
+                    margin: auto;
+                    display: flex;
+                    align-items: center;
                 }
                 .name {
                     margin-top: 5px;
